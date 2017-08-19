c4e8d7dbf5fd528b47528c2e453ab308ee703782
Edmund Cape <edmund@Edmunds-Mac.local>
2017-08-19 19:48:10 -0400

    enabled the presence of hidden files when creating the component data file.

---
+++ src/docs/examples/LabelE/ExampleOptional.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Label from 'ps-react/LabelE';
+
+/** Edmund's Optional label */
+export default function ExampleOptional() {
+  return <Label htmlFor="test" label="test" />
+}
